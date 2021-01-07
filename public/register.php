1fa00c2788ab763aa89db02208c75c9cf5ba4196
Jon <odilorg@gmail.com>
2021-01-07 16:15:58 +0500

    finished forms

--- public/register.php
+++ public/register.php
@@ -46,13 +46,13 @@
     <main class="form-signin">
       <form>
         <img class="mb-4" src="images/logo-library.png" alt="" width="72" height="57">
-        <h1 class="h3 mb-3 fw-normal">Пожалуйста войдите</h1>
-        <label for="inputEmail" class="visually-hidden">Email</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="visually-hidden">Password</label>
+        <h1 class="h3 mb-3 fw-normal">Пожалуйста зарегистрируйтесь</h1>
+        <input type="text" id="username" class="form-control" placeholder="Логин" nane="username" required autofocus>
+        <input type="text" id="firstName" class="form-control" placeholder="Имя" nane="firstName" required >
+        <input type="text" id="age" class="form-control" placeholder="Возраст" nane="age" required >
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required >
         <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required>
-
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Войти</button>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Отправить</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
       </form>
     </main>
